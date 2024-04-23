327a87bdea7d8d917b31569791bace1a901b60e9
alex-s168 <63254202+alex-s168@users.noreply.github.com>
2024-04-23 18:50:16 +0200

    ooohh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -e
 : "${CFLAGS:=-O2}"
 task=${1:-"all"}
 
-$CC $CFLAGS -DCC=\""$CC"\" -DCFLAGS=\""$CFLAGS"\" build.c -o build.exe
+$CC $CFLAGS -DCC=\""$CC"\" -DCC_ARGS=\""$CFLAGS"\" build.c -o build.exe
 ./build.exe $task
